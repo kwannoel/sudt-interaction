a2329d29cbe13d29bdbb2a6cf1b7fa3627d17687
Noel Kwan <noelkwan1998@gmail.com>
2021-11-10 22:43:54 +0800

    Fix tests

--- tests/src/tests.rs
+++ tests/src/tests.rs
@@ -1,130 +1,143 @@
 use super::*;
-use ckb_testtool::context::Context;
+use ckb_testtool::{builtin::ALWAYS_SUCCESS, context::Context};
 use ckb_testtool::ckb_types::{
     bytes::Bytes,
-    core::TransactionBuilder,
+    core::{Capacity, TransactionBuilder, TransactionView},
     packed::*,
     prelude::*,
 };
-use ckb_testtool::ckb_error::assert_error_eq;
-use ckb_testtool::ckb_script::ScriptError;
+use ckb_testtool::{ckb_error::assert_error_eq, ckb_script::ScriptError};
+use ckb_testtool::ckb_error;
 
-const MAX_CYCLES: u64 = 10_000_000;
+const MAX_CYCLES: u64 = 100_0000;
 
-// error numbers
-const ERROR_EMPTY_ARGS: i8 = 5;
 
-#[test]
-fn test_success() {
-    // deploy contract
+// errors
+const ERROR_AMOUNT: i8 = 5;
+
+fn build_test_context(
+    inputs_token: Vec<u128>,
+    outputs_token: Vec<u128>,
+    is_owner_mode: bool,
+) -> (Context, TransactionView) {
+    // deploy my-sudt script
     let mut context = Context::default();
-    let contract_bin: Bytes = Loader::default().load_binary("my-sudt");
-    let out_point = context.deploy_cell(contract_bin);
+    let sudt_bin: Bytes = Loader::default().load_binary("my-sudt");
+    let sudt_out_point = context.deploy_cell(sudt_bin);
+    // deploy always_success script
+    let always_success_out_point = context.deploy_cell(ALWAYS_SUCCESS.clone());
 
-    // prepare scripts
+    // build lock script
     let lock_script = context
-        .build_script(&out_point, Bytes::from(vec![42]))
+        .build_script(&always_success_out_point, Default::default())
         .expect("script");
     let lock_script_dep = CellDep::new_builder()
-        .out_point(out_point)
+        .out_point(always_success_out_point)
         .build();
 
-    // prepare cells
-    let input_out_point = context.create_cell(
-        CellOutput::new_builder()
-            .capacity(1000u64.pack())
-            .lock(lock_script.clone())
-            .build(),
-        Bytes::new(),
-    );
-    let input = CellInput::new_builder()
-        .previous_output(input_out_point)
-        .build();
-    let outputs = vec![
-        CellOutput::new_builder()
-            .capacity(500u64.pack())
-            .lock(lock_script.clone())
-            .build(),
-        CellOutput::new_builder()
-            .capacity(500u64.pack())
-            .lock(lock_script)
-            .build(),
-    ];
+    // build sudt script
+    let sudt_script_args: Bytes = if is_owner_mode {
+        // use always_success script hash as owner's lock
+        let lock_hash: [u8; 32] = lock_script.calc_script_hash().unpack();
+        lock_hash.to_vec().into()
+    } else {
+        // use zero hash as owner's lock which implies we can never enter owner mode
+        [0u8; 32].to_vec().into()
+    };
 
-    let outputs_data = vec![Bytes::new(); 2];
+    let sudt_script = context
+        .build_script(&sudt_out_point, sudt_script_args)
+        .expect("script");
+    let sudt_script_dep = CellDep::new_builder().out_point(sudt_out_point).build();
+
+    // prepare inputs
+    // assign 1000 Bytes to per input
+    let input_ckb = Capacity::bytes(1000).unwrap().as_u64();
+    let inputs = inputs_token.iter().map(|token| {
+        let input_out_point = context.create_cell(
+            CellOutput::new_builder()
+                .capacity(input_ckb.pack())
+                .lock(lock_script.clone())
+                .type_(Some(sudt_script.clone()).pack())
+                .build(),
+            token.to_le_bytes().to_vec().into(),
+        );
+        let input = CellInput::new_builder()
+            .previous_output(input_out_point)
+            .build();
+        input
+    });
+
+    // prepare outputs
+    let output_ckb = input_ckb * inputs_token.len() as u64 / outputs_token.len() as u64;
+    let outputs = outputs_token.iter().map(|_token| {
+        CellOutput::new_builder()
+            .capacity(output_ckb.pack())
+            .lock(lock_script.clone())
+            .type_(Some(sudt_script.clone()).pack())
+            .build()
+    });
+    let outputs_data: Vec<_> = outputs_token
+        .iter()
+        .map(|token| Bytes::from(token.to_le_bytes().to_vec()))
+        .collect();
 
     // build transaction
     let tx = TransactionBuilder::default()
-        .input(input)
+        .inputs(inputs)
         .outputs(outputs)
         .outputs_data(outputs_data.pack())
         .cell_dep(lock_script_dep)
+        .cell_dep(sudt_script_dep)
         .build();
+    (context, tx)
+}
+
+#[test]
+fn test_basic() {
+    let (mut context, tx) = build_test_context(vec![1000], vec![400, 600], false);
     let tx = context.complete_tx(tx);
 
     // run
     let cycles = context
         .verify_tx(&tx, MAX_CYCLES)
         .expect("pass verification");
-    println!("consume cycles: {}", cycles);
+    println!("cycles: {}", cycles);
 }
 
 #[test]
-fn test_empty_args() {
-    // deploy contract
-    let mut context = Context::default();
-    let contract_bin: Bytes = Loader::default().load_binary("my-sudt");
-    let out_point = context.deploy_cell(contract_bin);
-
-    // prepare scripts
-    let lock_script = context
-        .build_script(&out_point, Default::default())
-        .expect("script");
-    let lock_script_dep = CellDep::new_builder()
-        .out_point(out_point)
-        .build();
-
-    // prepare cells
-    let input_out_point = context.create_cell(
-        CellOutput::new_builder()
-            .capacity(1000u64.pack())
-            .lock(lock_script.clone())
-            .build(),
-        Bytes::new(),
-    );
-    let input = CellInput::new_builder()
-        .previous_output(input_out_point)
-        .build();
-    let outputs = vec![
-        CellOutput::new_builder()
-            .capacity(500u64.pack())
-            .lock(lock_script.clone())
-            .build(),
-        CellOutput::new_builder()
-            .capacity(500u64.pack())
-            .lock(lock_script)
-            .build(),
-    ];
-
-    let outputs_data = vec![Bytes::new(); 2];
-
-    // build transaction
-    let tx = TransactionBuilder::default()
-        .input(input)
-        .outputs(outputs)
-        .outputs_data(outputs_data.pack())
-        .cell_dep(lock_script_dep)
-        .build();
+fn test_destroy_udt() {
+    let (mut context, tx) = build_test_context(vec![1000], vec![800, 100, 50], false);
     let tx = context.complete_tx(tx);
 
     // run
-    let err = context
+    let cycles = context
         .verify_tx(&tx, MAX_CYCLES)
-        .unwrap_err();
-    // we expect an error raised from 0-indexed cell's lock script
-    let script_cell_index = 0;
-    assert_error_eq!(
-        err,
-        ScriptError::ValidationFailure(ERROR_EMPTY_ARGS).input_lock_script(script_cell_index)
-    );
+        .expect("pass verification");
+    println!("cycles: {}", cycles);
+}
+
+#[test]
+fn test_create_sudt_without_owner_mode() {
+    let (mut context, tx) = build_test_context(vec![1000], vec![1200], false);
+    let tx = context.complete_tx(tx);
+
+    // run
+    let err = context.verify_tx(&tx, MAX_CYCLES).unwrap_err();
+    // assert_error_eq!(err, ScriptError::ValidationFailure(ERROR_AMOUNT));
+    // TODO: Is there a way to cast error???
+    // The above example is outdated.
+    assert_error_eq!(err.kind(), ckb_error::ErrorKind::Script);
+}
+
+#[test]
+fn test_create_sudt_with_owner_mode() {
+    let (mut context, tx) = build_test_context(vec![1000], vec![1200], true);
+    let tx = context.complete_tx(tx);
+
+    // run
+    let cycles = context
+        .verify_tx(&tx, MAX_CYCLES)
+        .expect("pass verification");
+    println!("cycles: {}", cycles);
 }
